478b428dcb2b4605304f3581d6d3d0eac5e8a11a
Anthony Niño <anthonyninojose@gmail.com>
2023-03-28 17:43:44 -0400

    delete cards

--- src/components/Options/CardItem.jsx
+++ src/components/Options/CardItem.jsx
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types';
 
 export const CardItem = ({title,description}) => {
   return (
-    
+    <>
     <div className="col-7">
             <div className="card mt-3 cards">
                 <div className="card-body mt-n3">
-                    <h5 className="card-title text-start mt-0  fs-4 fw-semibold ms-2">{title} <i>Onichan</i></h5>
+                    <h5 className="card-title text-start fs-4 fw-semibold ms-2">{title} </h5>
                     <p className="card-tex text-start m-0 fs-5 ms-2 text-cards">{description}</p>
                 </div>
+                
             </div>
-        </div>
-    
+            
+    </div>
+   
+   </>
 
   )
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,5 +1,33 @@
-import React from 'react'
-import { Cards } from './Cards'
+import React from 'react';
+import { CardItem } from './CardItem';
+
+const card = [{
+  id: 1,
+  title: "CAMPUS COACH",
+  description: "Graduate knowing all of your options",
+}, 
+{   id: 2,
+  title: "JOB SEARCH",
+  description: "Resume, interviewing & identify your passions",},
+{   id: 3,
+  title: "GETTING A JOB",
+  description: "Negotiating an offer, promotion or reviewing a contract ",
+},{
+  id: 4,
+  title: "LEAVING A JOB",
+  description: "Voluntarily, involuntarily understanding right & severance package",
+},
+,{
+  id: 5,
+  title: "STARTING A FAMILY",
+  description: "Maternity, paternity, adopting and family leave rights",
+},
+,{
+  id: 6,
+  title: "WORKPLACE GUIDANCE",
+  description: "General help or navigating a questionable situation",
+}
+];
 
 export const Options = () => {
   return (
@@ -8,7 +36,13 @@ export const Options = () => {
         <h1 className="fs-1 fw-bold mb-4">Choose from the options below</h1>
         <div className='container'>
         <div className='row justify-content-center'>
-            <Cards/>
+        {
+        card.map( (card) => (
+          <CardItem key={card.id }
+          {...card} />
+        ))
+
+    }
         </div>
     </div>
     </div>
