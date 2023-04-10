980497531182989ebe7b4dddc12a3f20a2b24796
Anthony Niño <anthonyninojose@gmail.com>
2023-04-09 22:38:44 -0400

    Changes in CheckArea

--- src/components/FormCheckArea/CheckArea.jsx
+++ src/components/FormCheckArea/CheckArea.jsx
@@ -10,9 +10,9 @@ export const CheckArea = () => {
             </div>
             <div className='container mt-5'>
                 <div className='row justify-content-center mx-auto'>
-                      <div className="form-check col-xxl-5 col-xl-5 col-lg-7 col-md-9">
-                      <label className="list-group-item d-flex gap-2">
-                        <input className="form-check-input flex-shrink-0 border border-secondary" type="checkbox" value="" ></input>
+                      <div className="form-check ms-5 col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12">
+                      <label className="list-group-item d-flex gap-3">
+                        <input className="form-check-input mt-2 border border-secondary" type="checkbox" value="" ></input>
                         <span className='fw-bold text-start'>
                         CAMPUS COACH
                           <span className="d-block text-muted fw-normal">Graduate knowing all of your options</span>
@@ -21,9 +21,9 @@ export const CheckArea = () => {
                       </div>
                 </div>
                 <div className='row justify-content-center mx-auto mt-4'>
-                      <div className="form-check col-xxl-5 col-xl-5 col-lg-7 col-md-9">
-                      <label className="list-group-item d-flex gap-2">
-                        <input className="form-check-input flex-shrink-0 border border-secondary" type="checkbox" value="" ></input>
+                      <div className="form-check ms-5 col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12">
+                      <label className="list-group-item d-flex gap-3">
+                        <input className="form-check-input mt-2 border border-secondary" type="checkbox" value="" ></input>
                         <span className='fw-bold text-start'>
                         JOB SEARCH
                           <span className="d-block text-muted fw-normal">Resume, interviewing & identify your passions</span>
@@ -32,9 +32,9 @@ export const CheckArea = () => {
                       </div>
                 </div>
                 <div className='row justify-content-center mx-auto mt-4'>
-                      <div className="form-check col-xxl-5 col-xl-5 col-lg-7 col-md-9">
-                      <label className="list-group-item d-flex gap-2">
-                        <input className="form-check-input flex-shrink-0 border border-secondary" type="checkbox" value="" ></input>
+                      <div className="form-check ms-5 col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12">
+                      <label className="list-group-item d-flex gap-3">
+                        <input className="form-check-input mt-2 border border-secondary" type="checkbox" value="" ></input>
                         <span className='fw-bold text-start'>
                         GETTING A JOB
                           <span className="d-block text-muted fw-normal">Negotiating an offer, promotion or reviewing a contract</span>
@@ -43,9 +43,9 @@ export const CheckArea = () => {
                       </div>
                 </div>
                 <div className='row justify-content-center mx-auto mt-4'>
-                      <div className="form-check col-xxl-5 col-xl-5 col-lg-7 col-md-9">
-                      <label className="list-group-item d-flex gap-2">
-                        <input className="form-check-input flex-shrink-0 border border-secondary" type="checkbox" value="" ></input>
+                      <div className="form-check ms-5 col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12">
+                      <label className="list-group-item d-flex gap-3">
+                        <input className="form-check-input mt-2  border border-secondary" type="checkbox" value="" ></input>
                         <span className='fw-bold text-start'>
                         LEAVING A JOB
                           <span className="d-block text-muted fw-normal">Voluntarily, involuntarily, understanding rights & severance package</span>
@@ -54,20 +54,20 @@ export const CheckArea = () => {
                       </div>
                 </div>
                 <div className='row justify-content-center mx-auto mt-4'>
-                      <div className="form-check col-xxl-5 col-xl-5 col-lg-7 col-md-9">
-                      <label className="list-group-item d-flex gap-2">
-                        <input className="form-check-input flex-shrink-0 border border-secondary" type="checkbox" value="" ></input>
+                      <div className="form-check ms-5 col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12">
+                      <label className="list-group-item d-flex gap-3">
+                        <input className="form-check-input mt-2 border border-secondary" type="checkbox" value="" ></input>
                         <span className='fw-bold text-start'>
                         EXECUTIVE COACH
-                          <span className="d-block text-muted fw-normal">Successfully building a brand, leading a team & having work/life balance</span>
+                          <span className="d-block text-muted fw-normal mx-auto">Successfully building a brand, leading a team & having work/life balance</span>
                         </span>
                       </label>
                       </div>
                 </div>
                 <div className='row justify-content-center mx-auto mt-4'>
-                      <div className="form-check col-xxl-5 col-xl-5 col-lg-7 col-md-9">
-                      <label className="list-group-item d-flex gap-2">
-                        <input className="form-check-input flex-shrink-0 border border-secondary" type="checkbox" value="" ></input>
+                      <div className="form-check ms-5 col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12">
+                      <label className="list-group-item d-flex gap-3">
+                        <input className="form-check-input mt-2 border border-secondary" type="checkbox" value="" ></input>
                         <span className='fw-bold text-start'>
                         STARTING A FAMILY
                           <span className="d-block text-muted fw-normal">Maternity, paternity, adopting and family leave rights</span>
@@ -76,15 +76,21 @@ export const CheckArea = () => {
                       </div>
                 </div>
                 <div className='row justify-content-center mx-auto mt-4'>
-                      <div className="form-check col-xxl-5 col-xl-5 col-lg-7 col-md-9">
-                      <label className="list-group-item d-flex gap-2">
-                        <input className="form-check-input flex-shrink-0 border border-secondary" type="checkbox" value="" ></input>
+                      <div className="form-check ms-5 col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12">
+                      <label className="list-group-item d-flex gap-3">
+                        <input className="form-check-input mt-2 border border-secondary" type="checkbox" value="" ></input>
                         <span className='fw-bold text-start'>
                         WORKPLACE GUIDANCE
                           <span className="d-block text-muted fw-normal">General help or navigating a questionable situation</span>
                         </span>
                       </label>
+                      <div className='fs-5 mt-5 mb-4 btn-create rounded-pill'>
+                            <button className='btn text-light fs-5'>
+                                    Next
+                            </button>
                       </div>
+                      <a href="" className='mt-5 footer-text text-decoration-none'>Terms & Conditions</a>
+                    </div>
                 </div>
             </div>
         </div>
